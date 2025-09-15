a1e847bcdcb9d8bce310a4391d91723894576155
Sin <155725528+Chinnavit@users.noreply.github.com>
2025-09-16 05:24:41 +0700

    11: products

--- Desktop/Project Test/test 5/multitenant-ecommerce/src/trpc/routers/_app.ts
+++ Desktop/Project Test/test 5/multitenant-ecommerce/src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { createTRPCRouter } from '../init';
 
 import { authRounter } from '@/modules/auth/server/procedures';
+import { productsRouter } from '@/modules/products/server/procedures';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 
 export const appRouter = createTRPCRouter({
   auth: authRounter,
+  products: productsRouter,
   categories: categoriesRouter
 });
 // export type definition of API
