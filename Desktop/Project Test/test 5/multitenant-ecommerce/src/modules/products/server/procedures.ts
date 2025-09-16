1b3e7aff97ca990f115fb6d5dac5057bf066b391
Sin <155725528+Chinnavit@users.noreply.github.com>
2025-09-17 06:45:34 +0700

    12: filtering products

--- Desktop/Project Test/test 5/multitenant-ecommerce/src/modules/products/server/procedures.ts
+++ Desktop/Project Test/test 5/multitenant-ecommerce/src/modules/products/server/procedures.ts
@@ -9,10 +9,24 @@ export const productsRouter = createTRPCRouter ({
     .input(
         z.object({
             category: z.string().nullable().optional(),
+            minPrice: z.string().nullable().optional(),
+            maxPrice: z.string().nullable().optional(),
     }),
     )
     .query(async ({ ctx, input }) => {
         const where: Where = {};
+
+        if (input.minPrice){
+            where.price = {
+                greater_than_equal: input.minPrice
+            }
+        }
+        
+        if (input.maxPrice){
+            where.price = {
+                less_than_equal: input.maxPrice
+            }
+        }
 
         if (input.category){
             const categoriesData = await ctx.db.find({
@@ -27,8 +41,6 @@ export const productsRouter = createTRPCRouter ({
                     }
                 }
             });
-
-            console.log(JSON.stringify(categoriesData, null, 2));
 
             const formattedData = categoriesData.docs.map((doc)=>({
                     ...doc,
@@ -46,13 +58,11 @@ export const productsRouter = createTRPCRouter ({
                 subcategoriesSlugs.push(
                     ...parentCategory.subcategories.map(( subcategory ) => subcategory.slug)
                 )
+                where["category.slug"] = {
+                    in: [parentCategory.slug, ...subcategoriesSlugs],
             }
-
-            where["category.slug"] = {
-                in: [parentCategory.slug, ...subcategoriesSlugs],
-           }
         }
-
+    }
     const data = await ctx.db.find({
         collection: "products",
         depth: 1, // Populate "categories", "image"
@@ -62,3 +72,4 @@ export const productsRouter = createTRPCRouter ({
         return data;
     }),
 });
+
